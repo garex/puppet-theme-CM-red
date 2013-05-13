f13ab1afd372e616598a33be259f6a239b6fd8e7
Ustimenko Alexander <ustimenko.alexander@gmail.com>
2013-05-13 16:56:56 +0700

    Add time grouping tracker

--- javascripts/theme.js
+++ javascripts/theme.js
@@ -1,5 +1,5 @@
 Event.observe(document, 'dom:loaded', function() {
-	['Пользователь', 'Функция', 'Задача', 'Баг'].each(function(tracker) {
+	['Пользователь', 'Функция', 'Задача', 'Баг', 'Веха'].each(function(tracker) {
 		$$('table.list td.tracker:contains(' + tracker + ')').each(function(item) {
 		  $(item).update('').up().addClassName('tracker-' + tracker);
 		});
